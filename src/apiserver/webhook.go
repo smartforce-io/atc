cb474f52eb5d19cb1ed8b005288319b3ead49af6
Alim Fedoseev <amfedoseyev@gmail.com>
2020-09-03 14:51:47 +0600

    [DEMO] Showing received webhooks to logs

--- src/apiserver/webhook.go
+++ src/apiserver/webhook.go
@@ -1,8 +1,14 @@
 package apiserver
 
-import "net/http"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func (api *ActApiServer) webhook(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	log.Println("Reveived webhook:", string(body))
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Webhook was received!"))
 }
