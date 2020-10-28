4ee103b4562ee65a89ddad87afe86e33845c87f5
Alim Fedoseev <amfedoseyev@gmail.com>
2020-10-28 19:26:20 +0600

    Update a push action

--- src/githubservice/ghpush.go
+++ src/githubservice/ghpush.go
@@ -1,8 +1,22 @@
 package githubservice
 
-import "github.com/google/go-github/github"
+import (
+	"github.com/google/go-github/github"
+	"log"
+)
 
 func PushAction(push *github.WebHookPayload) {
+	const versionSource = "pom.xml"
 
+	for _, commit := range push.Commits {
+		for _, modified := range commit.Modified {
+			if modified == versionSource && isNewVersion(versionSource) {
+				log.Printf("There is a new version!!!!!")
+			}
+		}
+	}
+}
 
+func isNewVersion(s string) bool {
+	return true
 }
