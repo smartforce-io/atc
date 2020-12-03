fc7d34024d0b0095f762c198f138dcdbd1aa41b1
Alim Fedoseev <amfedoseyev@gmail.com>
2020-12-03 19:42:25 +0600

    Add a setting of port via invironment variable (for gcp)

--- src/main/main.go
+++ src/main/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"apiserver"
 	"log"
+	"os"
 )
 
 func main() {
 	log.Println("Automated Tag Creator")
-	apiserver.Instance().Start(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	apiserver.Instance().Start(":"+port)
 }
