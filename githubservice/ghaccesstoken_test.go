d58e38f2f35c18d31a5559c558d82748ff59fc45
DmitriyLewen <dmitriy.lewen@smartforce.io>
2021-10-08 12:00:35 +0600

    test: add test for ghaccesstoken add test errors for get pem files

--- githubservice/ghaccesstoken_test.go
+++ githubservice/ghaccesstoken_test.go
@@ -1,6 +1,7 @@
 package githubservice
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -30,6 +31,31 @@ func TestGetAccessTokenBasic(t *testing.T) {
 	}
 	if token != expected {
 		t.Errorf("Unexpected token, expected %s, got %s", expected, token)
-
 	}
+	//clear changes env for next tests
+	os.Setenv(envvars.PemData, "")
+}
+
+func TestErrorGetPemFromPemPathVariable(t *testing.T) {
+	mockClientProviderPtr := DefaultMockClientProvider()
+
+	var tests = []struct {
+		envvarsPem  string
+		expectedErr string
+	}{
+		{"", "path to .pem is empty"},
+		{"atcTest.pem", "open atcTest.pem: no such file or directory"},
+		{"../../atcTestEmpty.pem", "no .pem file"},
+	}
+	for _, test := range tests {
+		os.Setenv(envvars.PemPathVariable, test.envvarsPem)
+		_, err := getAccessToken(10, mockClientProviderPtr)
+
+		if fmt.Sprint(err) != test.expectedErr {
+			t.Errorf("No get err with PemPathVariable = \"%s\"\nexpectedErr: \"%v\", got: \"%v\"", test.envvarsPem, test.expectedErr, err)
+		}
+	}
+	//clear changes env for next tests
+	os.Setenv(envvars.PemPathVariable, "")
+
 }
